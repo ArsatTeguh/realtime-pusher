4b4a2a8e99c766dda63d141a117174d58d06c809
arsyatteguh <arsyatteguh@gmail.com>
2024-04-14 18:24:36 +0700

    fixed

--- api/index.js
+++ api/index.js
@@ -68,7 +68,7 @@ app.post('/action', (req, res) => {
 
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
